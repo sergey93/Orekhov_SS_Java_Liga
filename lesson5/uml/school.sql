4c677a5a87dffef5fbf3807e714a0f2eaf84a21c
Sergey Orekhov <lost9306@gmail.com>
2020-10-11 20:43:09 +0300

    добавил sql скрипт по 5 дз

---
+++ lesson5/uml/school.sql
@@ -0,0 +1,20 @@
+create table school (
+	name TEXT,
+	adress TEXT
+);
+
+create table teachers (
+	name TEXT,
+	age INT,
+	gender TEXT 
+);
+
+create table students (
+	name TEXT,
+	age INT,
+	gender TEXT 
+);
+
+create table subject (
+	title text
+);
